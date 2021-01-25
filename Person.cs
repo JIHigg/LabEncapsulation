b9614072db7955edadd6a2eb03477a8c0680ac9b
higginsdude@gmail.com <higginsdude@gmail.com>
2021-01-25 21:04:06 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace LabEncapsulation
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person()
+        {
+        }
+
+        public Person(string first, string last, int age)
+        {
+            this.firstName = first;
+            this.lastName = last;
+            this.age = age;
+            
+        }
+        public string FirstName { get { return this.firstName; } }
+        public string LastName { get { return this.lastName; } }
+        public int Age { get { return this.age; }}
+        
+       
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace LabEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            var lines = 5;
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                    .ThenBy(persons => p.Age)
+                    .ToList()
+                    .ForEach(persons => Console.WriteLine(persons.ToString()));
+        }
+    }
+}
